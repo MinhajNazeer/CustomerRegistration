bdb0881e5fed1a7c40941b4dfbbd6205313d86d3
MinhajNazeer <minhajnazeer395@gmail.com>
2023-05-12 00:11:44 +0530

    add all files

---
+++ database.js
@@ -0,0 +1,40 @@
+var sqlite3 = require('sqlite3').verbose()
+var md5 = require('md5')
+
+const DBSOURCE = "db.sqlite"
+
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+        // Cannot open database
+        console.error(err.message)
+        throw err
+    } else {
+        console.log('Connected to the SQlite database.')
+        db.run(`CREATE TABLE customer (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name text, 
+            address text,
+            email text,
+            dateOfBirth text,
+            gender text,
+            age INTEGER,
+            cardHolderName text,
+            cardNumber INTEGER,
+            expiryDate text,
+            cvv INTEGER,
+            timeStamp text
+            )`, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Deleted");
+            }
+        })
+        
+    }
+})
+
+module.exports = db
+
+
